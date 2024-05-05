c15f7ae1f296dd7654a2a366d6ecaf7220369903
Tony Stark <apurificato5680@gmai.com>
2024-05-05 18:51:05 -0400

    updated Models so their references for id are correct. updated index.js file for models so their associations are set.

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,36 +1,44 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Tag = require('./Tag');
+const Product = require('./Product');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+  ProductTag.init(
+    {
+      // id: {
+      //   type: DataTypes.INTEGER,
+      //   allowNull: false,
+      //   primaryKey: true,
+      //   autoIncrement: true
+      // },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Product, 
+          key: 'id',
+        },
+      },
+      tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Tag, 
+          key: 'id',
+        },
+      }
+  
     },
-    // Have to add product_id as an integer; references product model's id.
-    // product_id: {
-    //   type: DataTypes.INTEGER,
-      
-    // },
-    // Have to add tag_id as an integer; references tag model's id.
-    // tag_id: {
-    //   type: DataTypes.INTEGER,
-          
-    // },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product_tag',
+    }
+  )
 );
 
 module.exports = ProductTag;
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category
+  // ,{
+  // through: 'product_has_categories',
+  // foreignKey: 'product_id',
+  // }
+);
 
 // Categories have many Products
+Category.hasMany(Product
+  // ,{
+  // through: 'category_has_products',
+  // foreignKey: 'category_id',
+  // }
+);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'productTag',
+  foreignKey: 'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'productTag'
+  // foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,
@@ -19,14 +39,10 @@ module.exports = {
   ProductTag,
 };
 
-// Product.belongsToMany(Category, {
-//   through: 'product_has_categories',
-//   foreignKey: 'product_id',
-// });
-// Category.belongsToMany(Product, {
-//   through: 'category_has_products',
-//   foreignKey: 'category_id',
-// });
+Category.hasMany(Product, {
+  through: 'category_has_products',
+  foreignKey: 'category_id',
+});
 
 // Product belongs to Category, as a category can have multiple products but a product can only belong to one category.
 
