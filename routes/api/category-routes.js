95cd2574d6b68dd572f95dc81404b21af51016c6
Tony Stark <apurificato5680@gmai.com>
2024-05-05 20:34:42 -0400

    starting to set up api routes, so I can test these routes in insomnia.rest

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,32 +1,48 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models/');
 
-// The `/api/categories` endpoint
-
-router.get('/categories', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-  res.send('Categories found successfully')
-});
-
-router.get('/categories:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  res.send('Categories found by ID successfully')
-});
-
-router.post('/categories', async (req, res) => {
-  // create a new category in the database
+router.get('/categories', async (req, res) => {
   try {
-    const { id, category_name } = req.body
-    const category = new Category({ id, category_name })
-    await Category.create(category)
-} catch (err) {
-    console.log(err)
-    // Redirect user back to the categories page
-    res.redirect('/categories')
-}
+    const categories = await Category.findAll();
+    res.json(categories);
+  } catch (err) {
+    console.error(err);
+    // Error response to the client for failing to get categories
+    res.status(500).json({ error: 'Failed to fetch categories' });
+  }
 });
+
+router.get('/categories/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const category = await Category.findOne({ where: { id: categoryId } });
+    if (!category) {
+      // Error sent if category is not found
+      return res.status(404).json({ error: 'Category not found' });
+    }
+    res.json(category);
+  } catch (err) {
+    console.error(err);
+    // Error response to the client for failing to get the category
+    res.status(500).json({ error: 'Failed to fetch category' });
+  }
+});
+
+// Router to create a new category in the database:
+router.post('/categories', async (req, res) => {
+  try {
+    const { category_name } = req.body;
+    const category = new Category({ category_name })
+    await Category.create(category)
+    // Success response for created category
+    res.status(201).json(category);
+  } catch (err) {
+    console.error(err);
+    // Error response to the client when category is not created
+    res.status(500).json({ error: 'Failed to create category' });
+  }
+});
+
 
 router.put('/categories:id', (req, res) => {
   // update a category by its `id` value
